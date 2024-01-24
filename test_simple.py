43a0653306f7ba9a77ed74d41ee600d2f46b41e1
azhiglinskiy <zhiglanton@gmail.com>
2024-01-24 18:49:28 +0300

    added simple test from previous lesson

---
+++ test_simple.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
+
+try:
+    browser = webdriver.Chrome()
+    link = "https://SunInJuly.github.io/execute_script.html"
+    browser.get(link)
+    button = browser.find_element(By.TAG_NAME, "button")
+    button.click()
+
+except Exception as e:
+    print(e)
+
+finally:
+    browser.quit()
